bc68b18383c7af5bd524e4053b3722860fd17bf1
mikkel.ao <mikkel.ao.dev@gmail.com>
2024-11-08 19:56:00 +0100

    Uploading setup

---
+++ src/main/java/dk/cphbusiness/Main.java
@@ -0,0 +1,16 @@
+package dk.cphbusiness;
+import org.abstractica.javacsg.Geometry3D;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        JavaCSG csg = JavaCSGFactory.createNoCaching();
+
+
+        // csg.view();
+    }
+}
